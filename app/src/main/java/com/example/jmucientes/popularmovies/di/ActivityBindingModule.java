16143afaca5ab91f67321ddabade547bf224372e
Jose Mucientes <jmucientes@spotify.com>
2018-11-23 01:57:51 +0100

    Add missing Scope annotations

--- app/src/main/java/com/example/jmucientes/popularmovies/di/ActivityBindingModule.java
+++ app/src/main/java/com/example/jmucientes/popularmovies/di/ActivityBindingModule.java
@@ -2,6 +2,7 @@ package com.example.jmucientes.popularmovies.di;
 
 import com.example.jmucientes.popularmovies.MainActivity;
 import com.example.jmucientes.popularmovies.MoviesDetailsActivity;
+import com.example.jmucientes.popularmovies.di.scopes.ActivityScope;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
@@ -10,8 +11,10 @@ import dagger.android.ContributesAndroidInjector;
 public abstract class ActivityBindingModule {
 
     @ContributesAndroidInjector(modules = MainModule.class)
+    @ActivityScope
     abstract MainActivity mainActivity();
 
     @ContributesAndroidInjector(modules = MovieDetailsModule.class)
+    @ActivityScope
     abstract MoviesDetailsActivity movieDetails();
 }
